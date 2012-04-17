62876a23fa20f2ebf66e2fedcc24d8bf19f1d673
pablodip <pablodip@gmail.com>
2012-04-17 23:29:03 +0500

    added composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,4 @@
 <?php
 
-// autoloader
-require_once(__DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php');
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Mandango\Mondator'       => __DIR__.'/../src',
-    'Mandango\Mondator\Tests' => __DIR__,
-));
-$loader->registerPrefixes(array(
-    'Twig_' => __DIR__.'/../vendor/twig/lib',
-));
-$loader->register();
+$loader = require __DIR__.'/../vendor/.composer/autoload.php';
+$loader->add('Mandango\Mondator\Tests', __DIR__);
